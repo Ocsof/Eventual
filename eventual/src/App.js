63434e8a92beb7b9cafb032d766214916f2833fc
alessia.rocco <alessia.rocco@studio.unibo.it>
2022-08-24 18:54:12 +0200

    add react router dom Router

--- eventual/src/App.js
+++ eventual/src/App.js
@@ -1,18 +1,28 @@
 import './App.css';
 import SignUp from "./cmp/SignUp";
+import Login from "./cmp/Login";
+import {
+    BrowserRouter as Router,
+    Route,
+    NavLink,
+    Routes
+} from "react-router-dom";
 
 function App() {
   return (
       <div className="App">
           <aside>Eventual</aside>
-        <nav>
-            <h1>Login</h1>
-            <h1>Sign Up</h1>
-        </nav>
-        <header className="App-header">
-            {/*<Login />*/}
-            <SignUp />
-        </header>
+          <Router>
+              <NavLink to="/login">Login</NavLink>
+              <br/>
+              <NavLink to="/signup">Sign Up</NavLink>
+              <br/>
+              <hr />
+              <Routes>
+                  <Route exact path="/login" element={<Login/>} />
+                  <Route exact path="/signup" element={<SignUp />} />
+              </Routes>
+          </Router>
     </div>
   );
 }
