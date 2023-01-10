c249dc466f6fcb3468e89915aab44beb89fd3f50
Alessia Rocco <alessia.rocco@studio.unibo.it>
2023-01-11 00:12:53 +0100

    added media query

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -46,7 +46,6 @@ function App() {
                   <input
                       id="outlined-basic"
                       variant="outlined"
-                      fullWidth
                       label="Search"
                       onChange={inputHandler}
                   />
