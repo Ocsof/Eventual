93495eb0b89890eabf25c54fc9c60fb5939646fc
Ocsof <francesco.foschini6@studio.unibo.it>
2023-01-25 20:29:52 +0100

    Implemented a a first version of login and signup backend side with hashed password

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -3,56 +3,78 @@ const bcrypt = require('bcryptjs');
 // const jwt = require('jsonwebtoken'); //todo: valutarne l'utilizzo -> token di autenticazione e autorizzazione
 
 const UserModel = require('../models/userModel')(mongoose)
-const EventModel = mongoose.model("Events")
 
 exports.sign_user = (req,res)=>{
-    const email = req.body.email
-    const username = req.body.username
+    //const { name, surname, email, phone, password, category} = req.body;
+    const newuser = {
+        name: 'Andrea',
+        surname: 'Leonardi',
+        email: 'leo@normale.it',
+        phone: 33647,
+        password: "passwordPotente",
+        category: 'normale',
+        events: []
+    }
 
-    // controllo se l'email o il nome utente sono già presenti nel database
-    UserModel.findOne({ $or: [{ email }, { username }] }, (err, user) => {
+    // controllo se l'email è già presente nel database
+    UserModel.findOne({email: newuser.email}, (err, user) => {
         if (err) {
-            res.status(500).json({error: 'Errore del server'});
-        } else if (user) {
-            res.status(409).json({error: 'Email o nome utente già in uso'});
-        } else {
-            bcrypt.hash(req.body.password, 10)
-                .then(hash => {
-                    const user = new UserModel(req.body);
-                    user.save()
-                        .then(result => {
-                            res.status(201).json({
-                                message: 'Utente creato con successo!',
-                                result: result
-                            });
-                        })
-                        .catch(err => {
-                            res.status(500).json({
-                                error: err
-                            });
-                        });
-                });
+            res.status(500).send({error: 'Errore del server'});
+        } else if (user != null) {
+            res.status(409).send({error: 'Email utente già in uso'});
         }
+        bcrypt.hash(newuser.password, 10, (err, hash) => { //10 numero di round di cifratura
+            if (err) {
+                res.status(500).send(err);
+            }
+            /*
+            const newUser = new UserModel({
+                name,
+                surname,
+                email,
+                phone,
+                password: hash,
+                category,
+                events: []
+            });
+            */
+            const newUser = new UserModel({
+                name: newuser.name,
+                surname: newuser.surname,
+                email: newuser.email,
+                phone: newuser.phone,
+                password: hash,
+                category: newuser.category,
+                events: []
+            });
+            newUser.save((err) => {
+                if (err) {
+                    res.status(500).send(err);
+                }
+                res.send(`User ${newuser.name} ${newuser.surname} creato`);
+            });
+        })
     })
 }
 
-exports.log_user = (req, res)=>{
-    let fetchedUser;
-    UserModel.findOne({ email: req.body.email })
-        .then(user => {
-            if (!user) {
-                return res.status(401).json({
-                    message: 'Autenticazione fallita'
-                });
+exports.log_user = (req, res)=> {
+    UserModel.findOne({ email: "leo@normale.it" }, (err, user) => { //todo: sostituire con req.body.email
+        if (err){
+            res.status(500).send(err);
+        }
+        if (!user){
+            res.status(404).send('Utente non trovato');
+        }
+        bcrypt.compare("passwordPotente", user.password, (err, result) => { //todo: sostituire il primo con req.body.password
+            if (err){
+                res.status(500).send(err);
             }
-            fetchedUser = user;
-            return bcrypt.compare(req.body.password, user.password);
-        })
-        .catch(err => {
-            return res.status(401).json({
-                message: 'Autenticazione fallita'
-            });
+            if (!result){
+                res.status(401).send('Password errata');
+            }
+            res.status(200).send(`Benvenuto ${user.name} ${user.surname}`);
         });
+    })
 }
 
 /*** per aggiornare l'utente, es: iscrizione ad un evento **/
@@ -77,4 +99,5 @@ exports.read_myEvent = (req, res) => {
             res.json(user.events);
         })
 }
+
 
