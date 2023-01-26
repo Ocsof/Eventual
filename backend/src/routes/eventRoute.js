1a303dee67a68eb866283d152ab68723333276e8
Ocsof <francesco.foschini6@studio.unibo.it>
2023-01-26 15:55:49 +0100

    add read_event, update_event and delete event

--- backend/src/routes/eventRoute.js
+++ backend/src/routes/eventRoute.js
@@ -1,14 +1,18 @@
 const controller = require('../controllers/eventController');
 
 module.exports =  (server)=>{
-    server.route('/new_event')
+    server.route('/events')
+        .get(controller.read_tenMostRecentEvents)
         .post(controller.new_event)
+
+    server.route('/events/:id')
+        .get(controller.read_event)
+        .put(controller.update_event)
+        .delete(controller.delete_event); //solo admin e organizzatori
 
     server.route('/events/:category') //todo: cambiare sicuramente l'url
         .get(controller.read_eventsByCategory)
 
-    server.route('/home')
-        .get(controller.read_tenMostRecentEvents)
 
     server.route('/myusers')
         .get(controller.read_myUsers)
