370f7af903b8481d0d16f9f5858307d91f069f9f
Alessia Rocco <alessia.rocco@studio.unibo.it>
2023-02-02 23:10:28 +0100

    Restored problem of new event not uploading

--- frontend/src/cmp/events/NewEvent.tsx
+++ frontend/src/cmp/events/NewEvent.tsx
@@ -10,22 +10,43 @@ export function NewEvent() {
     const navigate = useNavigate();
 
     const [event, setEvent] = useState({
-        id: -1,
+        id: '',
         title: '',
-        author: JSON.parse(localStorage.getItem('user'))._id.toString(),
+        author: '',
         category: '',
         date: '',
         description: '',
+        price: ''
     })
 
-    function handleSave(){
+    function handleSave(e){
+        e.preventDefault()
+        const user =  JSON.parse(localStorage.getItem('user'))
+        const my_organizations = user.my_organizations
+
         axios.post("http://localhost:8082/events", event)
             .then(res =>{
                 console.log(res.data);
+                my_organizations.push(res.data._id)
                 NotificationManager.success("Event added: " + res.data._id);
             })
             .catch(error => console.error(error))
-        navigate("/events");
+
+        user.my_organizations = my_organizations
+        console.log(user.my_organizations)
+
+        axios.put('http://localhost:8082/user/'+ user._id, user)
+            .then(response => {
+                console.log(response.data);
+                if(response.status === 200){
+                    console.log(response.data)
+                    localStorage.setItem('user', JSON.stringify(user))
+                    navigate("/events");
+                }
+            })
+            .catch(error => {
+                console.log(error)
+            })
     }
 
     return (
@@ -40,22 +61,33 @@ export function NewEvent() {
                 <h3>New Event</h3>
             </Card.Title>
             <Card.Body className="d-flex flex-column">
-                <label className="form-label" htmlFor="newEventImg">Event image: </label>
-                <input style={{width: "100%"}} type="file" className="form-control" id="newEventImg" />
-                <label className="form-label" htmlFor="newEventTitle">title: </label>
-                <input style={{width: "100%"}} type="text" className="form-control" id="newEventTitle" onChange={(e) => setEvent({...event, title: e.target.value})}/>
-                <label className="form-label" htmlFor="newEventAuthor">author: </label>
-                <input style={{width: "100%"}} type="text" className="form-control" id="newEventAuthor" onChange={(e) => setEvent({...event, author: e.target.value})}/>
-                <label className="form-label" htmlFor="newEventCategory">category: </label>
-                <input style={{width: "100%"}} type="text" className="form-control" id="newEventCategory" onChange={(e) => setEvent({...event, category: e.target.value})}/>
-                <label className="form-label" htmlFor="newEventDate">date: </label>
-                <input style={{width: "100%"}} type="date" className="form-control" id="newEventDate" onChange={(e) => setEvent({...event, date: e.target.value})}/>
-                <label className="form-label" htmlFor="newEventDesc">description: </label>
-                <input style={{width: "100%"}} type="text" className="form-control" id="newEventDesc" onChange={(e) => setEvent({...event, description: e.target.value})}/>
+                <form onSubmit={handleSave}>
+                    <label className="form-label" htmlFor="newEventTitle">title: </label>
+                    <input required style={{width: "100%"}} type="text" className="form-control" id="newEventTitle" onChange={(e) => setEvent({...event, title: e.target.value})}/>
+                    <label className="form-label" htmlFor="newEventAuthor">author: </label>
+                    <input required style={{width: "100%"}} type="text" className="form-control" id="newEventAuthor" onChange={(e) => setEvent({...event, author: e.target.value})}/>
+                    <label className="form-label" htmlFor="newEventCategory">category: </label>
+                    <select
+                        className="form-control rounded"
+                        placeholder="Search by category..."
+                        id="category"
+                        onChange={(e) => setEvent({...event, category: e.target.value})}
+                    >
+                        <option value="party">Party</option>
+                        <option value="concert">Concert</option>
+                        <option value="birthday">Birthday</option>
+                    </select>
+                    <label className="form-label" htmlFor="newEventDate">date: </label>
+                    <input required style={{width: "100%"}} type="date" className="form-control" id="newEventDate" onChange={(e) => setEvent({...event, date: e.target.value})}/>
+                    <label className="form-label" htmlFor="newEventDesc">description: </label>
+                    <input required style={{width: "100%"}} type="text" className="form-control" id="newEventDesc" onChange={(e) => setEvent({...event, description: e.target.value})}/>
+                    <label className="form-label" htmlFor="newEventPrice">price: </label>
+                    <input required style={{width: "100%"}} type="number" className="form-control" id="newEventPrice" onChange={(e) => setEvent({...event, price: e.target.value})}/>
 
-                <Card.Footer >
-                    <Button style={{marginLeft: "90%"}} className="btn btn-success" onClick={handleSave}>Save</Button>
-                </Card.Footer>
+                    <Card.Footer >
+                        <Button style={{marginLeft: "90%"}} className="btn btn-success" type="submit" >Save</Button>
+                    </Card.Footer>
+                </form>
             </Card.Body>
         </Card>
     )
